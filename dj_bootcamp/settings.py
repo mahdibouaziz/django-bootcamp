b29d499d6a2dceb1322c0631718534c3fad35756
Mahdi <mahdibouaziz@hotmail.fr>
2020-12-23 13:09:20 +0100

    Day 1 finished: models + web app meanings

--- dj_bootcamp/settings.py
+++ dj_bootcamp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'profiles.apps.ProfilesConfig',
+    'products.apps.ProductsConfig',
+
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    #id=models.AutoField()
+    title=models.CharField(max_length=200)
+    content=models.TextField(null=True, blank=True)
+    price=models.IntegerField(default=0)
+
+    def __str__ (self):
+        return self.title
+        
+
+
